bfda15b106560ac9eb0728a4058de626f19b2161
AngielouIgop <sujedeangielou06@gmail.com>
2025-08-24 21:52:19 +0800

    updated index code

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ echo "<title>B.A.S.U.R.A. Rewards</title>";
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/uni-sidebar.css">
 </head>
 <body>
 
